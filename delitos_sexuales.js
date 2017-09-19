8b84466ae494fb563e794a950de2af1745b4266e
marianaviro <m.villamizar564@uniandes.edu.co>
2017-09-19 11:56:00 -0500

    Rutas de archivos actualizadas

--- delitos_sexuales.js
+++ delitos_sexuales.js
@@ -20,7 +20,7 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("delitos.csv")
+d3.csv("data/delitos.csv")
   .row(
     function(r) {
       return {
